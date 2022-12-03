257b546acee3751ec8c5413866fa30854b33a46f
Gaurav Mathur <gmathur@gurukul>
2022-12-02 22:21:01 -0800

    Cleaned up Day 3 solution a bit

--- src/main/scala/aoc2022/D03RucksackReorganization.scala
+++ src/main/scala/aoc2022/D03RucksackReorganization.scala
@@ -2,36 +2,37 @@ package aoc2022
 
 import utils.AocFileOps
 
-import scala.collection.mutable
 import scala.util.{Failure, Success}
 
 object D03RucksackReorganization extends App {
 
-  def charValue(ch: Char) = ch match {
+  def charValue(ch: Char): Int = ch match {
     case _ if ch >= 'a' && ch <= 'z' => ch - 'a' + 1
     case _ => ch - 'A' + 27
   }
 
   def solvePart2(input: List[String]): Long = {
-    val l: List[List[String]] = input.grouped(3).toList
-    l.foldLeft(0L) { (acc, group) =>
-      val groups: List[Set[Char]] = group.map(s => s.toCharArray.toSet)
-      val ans: Seq[Char] = groups.foldLeft(groups(0)) { (acc, s) => acc.intersect(s) }.toSeq
-      acc + charValue(ans(0))
+    val rucksacksInGroupsOf3 = input.grouped(3).toList
+
+    rucksacksInGroupsOf3.foldLeft(0L) { (acc, groupOf3Rucksacks) =>
+      val unqItemsInEach = groupOf3Rucksacks.map(_.toCharArray.toSet)
+      val badgeItemType = unqItemsInEach.foldLeft(unqItemsInEach.head) { (acc, s) => acc.intersect(s) }.toSeq
+      acc + charValue(badgeItemType.head)
     }
   }
 
   def solvePart1(input: List[String]): Long = {
-    input.foldLeft(0L) { (acc, contents) =>
-      val c = contents.toCharArray
-      val (leftC, rightC) = c.splitAt(c.length / 2)
-      val leftCSet = leftC.toSet
-      val rightCSet = rightC.toSet
-      val common = leftCSet.intersect(rightCSet).toSeq
-      acc + charValue(common.head)
+    input.foldLeft(0L) { (acc, rucksack) =>
+      val rucksackArr = rucksack.toCharArray
+      val (leftCompartment, rightCompartment) = rucksackArr.splitAt(rucksackArr.length / 2)
+      val leftCSet = leftCompartment.toSet
+      val rightCSet = rightCompartment.toSet
+      val commonBetweenCompartments = leftCSet.intersect(rightCSet).toSeq
+      acc + charValue(commonBetweenCompartments.head)
     }
   }
 
+  /// Test case
   val input = List(
     "vJrwpWtwJgWrhcsFMMfFFhFp",
     "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
@@ -42,6 +43,7 @@ object D03RucksackReorganization extends App {
   assert(157 == solvePart1(input))
   assert(70 == solvePart2(input))
 
+  /// Solve Part 1 and 2
   AocFileOps
     .readStringInputFromFile("src/main/resources/aoc2022/2022D03P1Input.lst") match {
     case Success(testVector) =>
