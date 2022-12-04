a24cf4a5cd56c2ae0c08246e3a6b957aae63600e
Gaurav Mathur <gmathur@gurukul>
2022-12-04 10:49:58 -0800

    Solved Day 04 part 2

--- src/main/scala/aoc2022/D04CampCleanup.scala
+++ src/main/scala/aoc2022/D04CampCleanup.scala
@@ -5,19 +5,33 @@ import utils.AocFileOps
 import scala.util.{Failure, Success}
 
 object D04CampCleanup extends App {
+  type SectionPair = Array[Int]
+  type SectionPairs = (Array[Int], Array[Int])
+
+  private def doParts(line: String): SectionPairs = {
+    val parts = line.split(",")
+    val leftPair: SectionPair = parts(0).split("-").map(_.toInt)
+    val rightPair: SectionPair = parts(1).split("-").map(_.toInt)
+    (leftPair, rightPair)
+  }
+
+  def solvePart2(input: List[String]): Long = {
+    input.foldLeft(0L) { (acc, line) =>
+      val (leftPair, rightPair) = doParts(line)
+
+      if (leftPair(0) <= rightPair(0) && leftPair(1) >= rightPair(0))  { acc + 1 }
+      else if (rightPair(0) <= leftPair(0) && rightPair(1) >= leftPair(0)) { acc + 1 }
+      else { acc }
+    }
+  }
+
   def solvePart1(input: List[String]): Long = {
     input.foldLeft(0L) { (acc, line) =>
-      val parts = line.split(",")
-      val range1: Array[Int] = parts(0).split("-").map(_.toInt)
-      val range2: Array[Int] = parts(1).split("-").map(_.toInt)
+      val (leftPair, rightPair) = doParts(line)
 
-      if (range1(0) >= range2(0) && range1(1) <= range2(1)) {
-        acc + 1
-      } else if (range2(0) >= range1(0) && range2(1) <= range1(1)) {
-        acc + 1
-      } else {
-        acc
-      }
+      if (leftPair(0) >= rightPair(0) && leftPair(1) <= rightPair(1)) { acc + 1 }
+      else if (rightPair(0) >= leftPair(0) && rightPair(1) <= leftPair(1)) { acc + 1 }
+      else { acc }
     }
   }
 
@@ -31,12 +45,14 @@ object D04CampCleanup extends App {
     "2-6,4-8"
   )
   assert(2 == solvePart1(testInput))
+  assert(4 == solvePart2(testInput))
 
   /// Solve Part 1 and 2
   AocFileOps
     .readStringInputFromFile("src/main/resources/aoc2022/2022D04Input.lst") match {
     case Success(testVector) =>
       assert(515 == solvePart1(testVector))
+      assert(883 == solvePart2(testVector))
     case Failure(exception) => println(s"Error parsing test input (error: ${exception})")
   }
 }
