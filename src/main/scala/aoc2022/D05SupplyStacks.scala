043001aa137ce2e29a70bed5a78ee6e9a3319f3d
Gaurav Mathur <gmathur@gurukul>
2022-12-05 08:46:27 -0800

    Refactored to make it look cleaner

--- src/main/scala/aoc2022/D05SupplyStacks.scala
+++ src/main/scala/aoc2022/D05SupplyStacks.scala
@@ -13,54 +13,60 @@ object D05SupplyStacks extends App {
       Move(moveInfo(1).toInt, moveInfo(3).toInt, moveInfo(5).toInt)
     }
   }
+
+  type NumOfCrateStacks = Int
   type CrateStacksWithIdent = Map[Int, mutable.Stack[Char]]
   type Moves = Array[Move]
 
-  def parse(input: String): (Int, CrateStacksWithIdent, Moves) = {
+  def parse(input: String): (NumOfCrateStacks, CrateStacksWithIdent, Moves) = {
     val cratesAndMoves: Array[String] = input.split("\n\n")
 
     val crates = cratesAndMoves(0).split("\n")
     val max = crates.last.split("\\W+").last.toInt
 
-    val crateStacksWithIdent = (1 to (max))
+    val crateStacks = (1 to (max))
       .map(_ -> mutable.Stack[Char]()) // indexed sequence
       .toMap // tuple sequence to Map
     val topToBottomHorizontalCrateView = crates.dropRight(1)
-      .map(_.grouped(4)
-        .map(_(1)).toSeq)
-    topToBottomHorizontalCrateView
+      .map(
+        _.grouped(4)
+        .map(_(1))
+        .toSeq
+      )
+
+    topToBottomHorizontalCrateView // side-effecting
       .reverse // Bottom to top horizontal crate view
       .map { _.zipWithIndex.filter { case (ch, _) => ch != ' ' }
-        .map { case (crate, crateStackIdent) => crateStacksWithIdent(crateStackIdent+1).push(crate) }
+        .map { case (crate, crateStackIdent) => crateStacks(crateStackIdent+1).push(crate) }
       }
     val moves: Array[Move] = cratesAndMoves(1).split("\n").map(Move(_))
-    (max, crateStacksWithIdent, moves)
+    (max, crateStacks, moves)
   }
 
   def solvePart2(input: String): String = {
-    val cm = parse(input)
-    cm._3.foreach { move =>
-      val crateStacksWithIdent = cm._2
+    val createAndMoveInfoTuple = parse(input)
+    createAndMoveInfoTuple._3.foreach { move => // side-effecting
+      val crateStacks = createAndMoveInfoTuple._2
       val cratesToMove = (1 to move.num).foldLeft("") { (acc, i) =>
-        val fromStack = crateStacksWithIdent(move.from)
+        val fromStack = crateStacks(move.from)
         acc + fromStack.pop()
       }
-      cratesToMove.reverse.foreach { crate => crateStacksWithIdent(move.to).push(crate) }
+      cratesToMove.reverse.foreach { crate => crateStacks(move.to).push(crate) }
     }
-    (1 to cm._1).map(cm._2(_).top).mkString("")
+    (1 to createAndMoveInfoTuple._1).map(createAndMoveInfoTuple._2(_).top).mkString("")
   }
 
   def solvePart1(input: String): String = {
-    val cm = parse(input)
-    cm._3.foreach { move =>
-      val crateStacksWithIdent = cm._2
-      (1 to move.num).foreach { i =>
-        val fromStack = crateStacksWithIdent(move.from)
-        val toStack = crateStacksWithIdent(move.to)
+    val createAndMoveInfoTuple = parse(input)
+    createAndMoveInfoTuple._3.foreach { move =>
+      val crateStacks = createAndMoveInfoTuple._2
+      (1 to move.num).foreach { _ => // side-effecting
+        val fromStack = crateStacks(move.from)
+        val toStack = crateStacks(move.to)
         toStack.push(fromStack.pop())
       }
     }
-    (1 to cm._1).map(cm._2(_).top).mkString("")
+    (1 to createAndMoveInfoTuple._1).map(createAndMoveInfoTuple._2(_).top).mkString("")
   }
 
   AocFileOps
