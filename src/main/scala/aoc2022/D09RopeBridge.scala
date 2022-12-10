f487894a0230201bf2a001aa9a16f718a0b41677
Gaurav Mathur <gmathur@gurukul>
2022-12-10 10:31:29 -0800

    Solved Day 9 part 2

--- src/main/scala/aoc2022/D09RopeBridge.scala
+++ src/main/scala/aoc2022/D09RopeBridge.scala
@@ -14,52 +14,65 @@ object D09RopeBridge extends App {
       case s"U $step" => 1.to(step.toInt).foldLeft(List[(Int, Int)]()) { case (acc, _) => acc :+ (0, 1) }
     }
 
-  trait Location {
+  trait Coor {
     def x: Int
     def y: Int
-  }
-  case class HLocation(x: Int, y: Int) extends Location {
-    def move(xDis: Int, yDis: Int) = HLocation(x + xDis, y + yDis)
-  }
-  case class TLocation(x: Int, y: Int) extends Location
+    def move(xDis: Int, yDis: Int): Coor
 
-  def solveDay1(input: List[String]): Int = {
+  }
+  case class HCoor(x: Int, y: Int) extends Coor {
+    def move(xDis: Int, yDis: Int): HCoor = HCoor(x + xDis, y + yDis)
+  }
+  case class TCoor(x: Int, y: Int) extends Coor {
+    def move(xDis: Int, yDis: Int): TCoor = TCoor(x + xDis, y + yDis)
+  }
+
+  def solve(input: List[String], ropeLength: Int): Int = {
     val l = parse(input)
-    val visited = mutable.Set[TLocation]()
+    val visited = mutable.Set[Coor]()
 
-    val rope = Vector(HLocation(0, 0)) ++ (1 to 10).foldLeft(Vector[Location]()){ (acc, _ ) => acc :+ TLocation(0, 0) }
-    val finalLocation = l.foldLeft((HLocation(0, 0), TLocation(0, 0))) { (locations, displacement) =>
-      val hLocation = locations._1.move(displacement._1, displacement._2)
-      val tLocation = {
-        val hCurrX = hLocation.x
-        val hCurrY = hLocation.y
-        val tCurrX = locations._2.x
-        val tCurrY = locations._2.y
+    val foldedRope =
+      Vector(HCoor(0, 0)) ++  // Head
+      (1 until ropeLength).foldLeft(Vector[Coor]()) { (acc, _) => acc :+ TCoor(0, 0) } // Tail of length ropeLength - 1
 
-        (hCurrX - tCurrX, hCurrY - tCurrY) match {
-          case (x, y) if x == 0 && y == 0 => TLocation(tCurrX, tCurrY)
-          case (x, y) if Math.abs(x) <= 1 && Math.abs(y) <= 1 => TLocation(tCurrX, tCurrY)
-          case (x, y) =>
-            val applyX = if (x == 0) 0 else if (x == -2 ) -1 else if (x == 2) 1 else x
-            val applyY = if (y == 0) 0 else if (y == -2 ) -1 else if (y == 2) 1 else y
-            TLocation(tCurrX + applyX, tCurrY + applyY)
+    l.foldLeft(foldedRope) { (rope, headDisplacement) =>
+      val hLocation = rope.head.move(headDisplacement._1, headDisplacement._2)
+
+      val movedRope = rope.tail.scanLeft(hLocation) { (prevKnotLoc: Coor, thisKnotLoc: Coor) =>
+        val tLocation = {
+          val (hCurrX, hCurrY, tCurrX, tCurrY) = (prevKnotLoc.x, prevKnotLoc.y, thisKnotLoc.x, thisKnotLoc.y)
+
+          (hCurrX - tCurrX, hCurrY - tCurrY) match {
+            case (x, y) if x == 0 && y == 0 => TCoor(tCurrX, tCurrY)
+            case (x, y) if Math.abs(x) <= 1 && Math.abs(y) <= 1 => TCoor(tCurrX, tCurrY)
+            case (x, y) =>
+              val applyX = if (x == 0) 0 else if (x == -2) -1 else if (x == 2) 1 else x
+              val applyY = if (y == 0) 0 else if (y == -2) -1 else if (y == 2) 1 else y
+              TCoor(tCurrX + applyX, tCurrY + applyY)
           }
         }
-      visited.add(tLocation)
-      (hLocation, tLocation)
+        tLocation
       }
+      visited.add(movedRope.last)
+      movedRope
+    }
     visited.size
   }
 
+  def solvePart1(input: List[String]) = solve(input, 2)
+  def solvePart2(input: List[String]) = solve(input, 10)
+
   AocFileOps
     .readInputAsStringList("src/main/resources/aoc2022/2022D09TestInput.lst") match {
-    case Success(input) => assert(13 == solveDay1(input))
+    case Success(input) => assert(13 == solvePart1(input))
     case Failure(exception) => println(s"Error parsing test input (error: ${exception})")
   }
 
   AocFileOps
     .readInputAsStringList("src/main/resources/aoc2022/2022D09Input.lst") match {
-    case Success(input) => assert(5960 == solveDay1(input))
+    case Success(input) =>
+      assert(5960 == solvePart1(input))
+      assert(2327 == solvePart2(input))
     case Failure(exception) => println(s"Error parsing test input (error: ${exception})")
   }
 }
