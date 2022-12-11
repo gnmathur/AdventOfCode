3f77dca8f678dec89ee6172a3415ad633279c2b2
Gaurav Mathur <gmathur@gurukul>
2022-12-10 22:45:28 -0800

    Solved day 10 part 2

--- src/main/scala/aoc2022/D10CathodeRayTube.scala
+++ src/main/scala/aoc2022/D10CathodeRayTube.scala
@@ -12,27 +12,43 @@ object D10CathodeRayTube extends App {
   case class Add(operand: Option[Int]) extends Instruction
   case class Noop(operand:Option[Int]) extends Instruction
 
-  case class Register(v: Long)
+  // Register value
+  case class Register(v: Long) extends AnyVal
+  // The cycle the current instruction is on
   case class InstructionCycles(v: Int) extends AnyVal
+  // Accumulate signal strength values
   type SignalStrength = Map[Int, Long]
 
+  def lightUp(cycle: Int, xPos: Long) = {
+    if (cycle >= xPos - 1 && cycle <= xPos + 1) true else false
+  }
+
   @tailrec
-  def doCycle(program: List[Instruction], ic: InstructionCycles, r: Register, cycleNum: Int, ss: SignalStrength): SignalStrength =
-    if (program.isEmpty) ss else {
+  def doCycle(
+               program: List[Instruction],
+               ic: InstructionCycles,
+               register: Register,
+               cycleNum: Int,
+               sigStrength: SignalStrength,
+               fb: Vector[Char]
+             ): (SignalStrength, Vector[Char]) =
+    if (program.isEmpty) (sigStrength, fb) else {
+      val newFb = if (lightUp((cycleNum-1)%40, register.v)) fb :+ '#' else fb :+ '.'
       val newCycle = cycleNum + 1
-      val newSS = if (cycleNum % 20 == 0) ss + (cycleNum -> cycleNum * r.v) else ss
+      val newSS = if (cycleNum % 20 == 0) sigStrength + (cycleNum -> cycleNum * register.v) else sigStrength
 
       program.head match {
         case Add(operand) =>
           val newProgram = if (ic.v == 2) program.tail else program
           val newIc = if (ic.v == 2) InstructionCycles(1) else InstructionCycles(ic.v +1 )
-          val newRegister = if (ic.v == 2) Register(r.v + operand.get) else r
-          doCycle(newProgram, newIc, newRegister, newCycle, newSS)
+          val newRegister = if (ic.v == 2) Register(register.v + operand.get) else register
+          doCycle(newProgram, newIc, newRegister, newCycle, newSS, newFb)
+
         case Noop(_) =>
           val newProgram = if (ic.v == 1) program.tail else program
           val newIc = InstructionCycles(1)
-          val newRegister = r
-          doCycle(newProgram, newIc, newRegister, newCycle, newSS)
+          val newRegister = register
+          doCycle(newProgram, newIc, newRegister, newCycle, newSS, newFb)
       }
     }
 
@@ -44,11 +60,21 @@ object D10CathodeRayTube extends App {
       case noopRegex => Noop(None)
   }
 
-  def solvePart1(input: List[String]) = {
-    val ss = doCycle(parse(input), InstructionCycles(1), Register(1), 1, Map())
-    ss(20) + ss(60) + ss(100) + ss(140) + ss(180) + ss(220)
+  def drawFb(fb: Vector[Char]): Unit = {
+    fb.zipWithIndex.foreach { case (pixel, idx) =>
+      if (idx % 40 == 0) {
+        println("\n" + pixel)
+      } else {
+        print(pixel)
+      }
+    }
   }
 
+  def solvePart1(input: List[String]) = {
+    val (ss, fb) = doCycle(parse(input), InstructionCycles(1), Register(1), 1, Map(), Vector())
+    drawFb(fb)
+    ss(20) + ss(60) + ss(100) + ss(140) + ss(180) + ss(220)
+  }
 
   // Test cases
   AocFileOps
@@ -61,6 +87,8 @@ object D10CathodeRayTube extends App {
   AocFileOps
     .readInputAsStringList("src/main/resources/aoc2022/2022D10Input.lst") match {
     case Success(input) => assert(14620 == solvePart1(input))
+    // Frame buffer draws BJFRHRFU
     case Failure(exception) => println(s"Error parsing test input (error: ${exception})")
   }
+
 }
